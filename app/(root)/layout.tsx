7f7c1d0cd91182b4beb7dc2c03af4b081973f1d2
ola-nishant <nishant.4716403221@ipu.ac.in>
2024-04-06 14:58:37 +0530

    added first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,35 @@
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
+import '../globals.css'
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+export const metadata = {
+    title : 'Threads',
+    description : "A NextJS Social media application"
+}
+
+const inter = Inter({ subsets : ["latin"]})
+
+export default function RootLayout({ children }:{children: React.ReactNode}){
+    return (
+    <ClerkProvider>
+        <html lang = "en">
+            <body className={`${inter.className} bg-dark-1`}>
+                <Topbar/>
+                <main className='flex flex-row'>
+                    <LeftSidebar/>
+                    <section className="main-container">
+                        <div className='w-full max-w-4xl'>
+                            {children}
+                        </div>
+                    </section>
+                    <RightSidebar/>
+                </main>
+                <Bottombar/>
+            </body>
+        </html>
+    </ClerkProvider>
+    )
+}
